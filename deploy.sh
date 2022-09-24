e19055bf46ce4a46955b61d255196ea59b1fea12
sambles <sambles@users.noreply.github.com>
2022-09-24 13:59:19 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -639,7 +639,7 @@ case "$deploy_type" in
     done
 
     update_kubectl_cluster
-    $UPLOAD_MODEL_DATA -c "mv meta-data/* ." -C "$OASIS_PIWIND_DIR" OasisLMF/PiWind/1 ${files_to_copy[@]}
+    $UPLOAD_MODEL_DATA -c "cp meta-data/* ." -C "$OASIS_PIWIND_DIR" OasisLMF/PiWind/1 ${files_to_copy[@]}
   ;;
   "analyses")
 
